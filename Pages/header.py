98c4797259d4a42e4093f05f1a0b4873118a08fe
arusyakmin <arusyak.minassian@gmail.com>
2024-02-03 02:12:01 +0400

    7-rd yerazy

--- Pages/header.py
+++ Pages/header.py
@@ -11,8 +11,7 @@ class header6PMPage(Helper):
     search_input = (By.CSS_SELECTOR,"input[placeholder = 'Search 6pm.com']")
     search_btn = (By.XPATH,"//form[@id = 'searchForm']//button")
     login_icon = (By.XPATH, "//div[@class='z-z']/a[contains(@href, 'account')]")
-
-
+    btn_search = (By.XPATH, "//button[normalize-space()='Submit Search']")
     def open_login_page(self):
         try:
             self.wait_and_click(self.login_icon)
@@ -23,9 +22,10 @@ class header6PMPage(Helper):
 
     def search_items(self):
         try:
-            search_field = self.find_and_send_keys(self.search_input, test_data.search_text)
+            inp = self.find_and_send_keys(self.search_input, test_data.search_text)
             time.sleep(2)
-            search_field.send_keys(Keys.ENTER)
+            self.wait_and_click(self.btn_search)
+            time.sleep(2)
             # self.find_and_click(self.search_btn)
         except Exception as e:
             logging.info(f"There was an error, {e}")
