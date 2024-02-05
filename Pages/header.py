7acfe51a50a01180534203d72ded9de2be7856b0
arusyakmin <arusyak.minassian@gmail.com>
2024-02-05 12:12:11 +0400

    nonor

--- Pages/header.py
+++ Pages/header.py
@@ -4,15 +4,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import my_config 
 from Helpers.general_helpers import GeneralHelper as Helper
-import logging
+import logging, time
 import TestData.test_data as test_data
 
 class header6PMPage(Helper):
     search_input = (By.CSS_SELECTOR,"input[placeholder = 'Search 6pm.com']")
     search_btn = (By.XPATH,"//form[@id = 'searchForm']//button")
     login_icon = (By.XPATH, "//div[@class='z-z']/a[contains(@href, 'account')]")
-
-
+    btn_search = (By.XPATH, "//button[normalize-space()='Submit Search']")
     def open_login_page(self):
         try:
             self.wait_and_click(self.login_icon)
@@ -23,7 +22,10 @@ class header6PMPage(Helper):
 
     def search_items(self):
         try:
-            self.find_and_send_keys(self.search_input, test_data.search_text)
-            self.find_and_click(self.search_btn)
+            inp = self.find_and_send_keys(self.search_input, test_data.search_text)
+            time.sleep(2)
+            self.wait_and_click(self.btn_search)
+            time.sleep(2)
+            # self.find_and_click(self.search_btn)
         except Exception as e:
             logging.info(f"There was an error, {e}")
